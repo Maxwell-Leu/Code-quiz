c910d86ba3a649867539d6d0ca53e98d9190be1b
Maxwell L <mleuthner2002@gmail.com>
2023-05-25 10:28:54 -0500

    moved questions and made question info display
    variable questions broke my timer function when trying to import so it was moved to scripts

---
+++ assests/javascript/questions.js
@@ -0,0 +1,24 @@
+const questions =[
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "3"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "2"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "1"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "4"
+    }
+];
+
+export {questions};
--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,20 +1,57 @@
 var timerEl = document.querySelector(".timer");
 var welcomeEl = document.querySelector(".welcome");
 var questionEl = document.querySelector(".question");
-var timeLeft;
-var currentQuestion = 0;
+var timeLeft = 75;
+var currentQuestion = -1;
+var questionsAnswered = 0;
+var questions =[
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "3"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "2"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "1"
+    },
+    {
+        question: "what",
+        choices: ["1", "2", "3", "4"],
+        answer: "4"
+    }
+];
 
 
 function timer(){
     welcomeEl.setAttribute("style","display:none;");
     questionEl.setAttribute("style", "display:flex");
-    timeLeft = 75;
+    changeQuestion();
     var timerInterval = setInterval(function(){
         timerEl.textContent = "Timer: " + timeLeft;
         if(timeLeft === 0){
             clearInterval(timerInterval);
-
         }
         timeLeft--;
     }, 1000)
+}
+
+//import { questions } from "./questions";
+function changeQuestion(){
+    if(currentQuestion < questionsAnswered){
+        questionEl.children[0].textContent = questions[questionsAnswered].question;
+        for(i = 0; i < 4; i++){
+            if(questions[questionsAnswered].choices[i] === questions[questionsAnswered].answer){
+                questionEl.children[i + 1].textContent = questions[questionsAnswered].choices[i];
+            }else{
+                questionEl.children[i + 1].textContent = questions[questionsAnswered].choices[i];
+            }
+            
+        }
+    }
 }

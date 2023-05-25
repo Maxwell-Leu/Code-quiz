7339b37445b64d619666156e4ccdd1bf17259007
Maxwell L <mleuthner2002@gmail.com>
2023-05-24 20:31:34 -0500

    hid welcome screen on click

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,10 +1,11 @@
 var timerEl = document.querySelector(".timer");
-var questionEl = document.querySelector(".question");
+var welcomeEl = document.querySelector(".welcome");
 var timeLeft;
 var currentQuestion = 0;
 
 
 function timer(){
+    welcomeEl.setAttribute("style","display:none;")
     timeLeft = 75;
     var timerInterval = setInterval(function(){
         timerEl.textContent = "Timer: " + timeLeft;

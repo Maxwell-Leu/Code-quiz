5859b3019f43c56e6d7eabb376c4dd5f0f47cc52
Maxwell L <mleuthner2002@gmail.com>
2023-05-24 23:34:32 -0500

    layout can be changed to the first question

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,11 +1,13 @@
 var timerEl = document.querySelector(".timer");
 var welcomeEl = document.querySelector(".welcome");
+var questionEl = document.querySelector(".question");
 var timeLeft;
 var currentQuestion = 0;
 
 
 function timer(){
-    welcomeEl.setAttribute("style","display:none;")
+    welcomeEl.setAttribute("style","display:none;");
+    questionEl.setAttribute("style", "display:flex");
     timeLeft = 75;
     var timerInterval = setInterval(function(){
         timerEl.textContent = "Timer: " + timeLeft;
